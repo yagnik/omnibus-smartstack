248c036ff6b51eedb51a1d82fba2b1f8dbc8a6aa
Yagnik <yagnikkhanna@gmail.com>
2016-05-08 17:07:39 +0530

    Update dendrite

--- config/projects/smartstack.rb
+++ config/projects/smartstack.rb
@@ -1,6 +1,6 @@
 name "smartstack"
 maintainer "Yagnik"
-homepage "https://github.com/airbnb/synapse"
+homepage "https://github.com/yagnik/omnibus-smartstack"
 
 install_dir "#{default_root}/#{name}"
 
@@ -8,16 +8,19 @@ build_version do
   source :git
   output_format :git_describe
 end
+
 build_iteration 1
 override :ruby,         version: "2.2.4"
 override :bundler,      version: "1.11.2"
 override :rubygems,     version: "2.5.2"
 override :synapse,      version: "0.13.1"
 override :nerve,        version: "0.7.0"
+override :dendrite,     version: "0.3.0"
 
 dependency "preparation"
 dependency "synapse"
 dependency "nerve"
+dependency "dendrite"
 dependency "haproxy"
 dependency "version-manifest"
 
---
+++ config/software/dendrite.rb
@@ -0,0 +1,10 @@
+name "dendrite"
+default_version "0.3.0"
+
+
+dependency "ruby"
+dependency "rubygems"
+
+build do
+  gem "install dendrite -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
+end
